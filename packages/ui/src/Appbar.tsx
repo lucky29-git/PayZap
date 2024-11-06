9433e114626a488bc69e36b7b9289098a66c7293
lucky29-git <chandrashekharchoudha@gmail.com>
2024-11-06 10:34:54 +0530

    added recoil

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -1,18 +1,29 @@
 import { signIn, signOut, useSession } from 'next-auth/react'
+import { Button } from './button'
 
-export const Appbar = () => {
+interface AppbarProps{
+    user?: {
+        name?: string | null
+    },
+    // TODO : put types of onSignin and onSignout 
+    onSignin : any,
+    onSignout : any
+}
+
+export const Appbar = ({
+    user, 
+    onSignin,
+    onSignout
+} : AppbarProps) => {
+
     const session = useSession()
-    return <div>
-        <button onClick={ () => signIn()}>
-            Sign in
-        </button>
-        <br />
-        <br />
-        <button onClick = { () => signOut()}>
-            Logout
-        </button>
-        <br />
-        <br />
-        {JSON.stringify(session)}
+    
+    return <div className="flex justify-between border-b px-4">
+    <div className="text-lg flex flex-col justify-center">
+        PayZap
     </div>
+    <div className="flex flex-col justify-center pt-2">
+        <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
+    </div>
+</div>
 }
