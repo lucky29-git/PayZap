2c1dd14dbe8d11d18d0b38361014afff3d9cb9cb
lucky29-git <chandrashekharchoudha@gmail.com>
2024-11-10 09:31:30 +0530

    fix error while building

--- apps/user-app/app/api/auth/[...nextauth]/route.ts
+++ apps/user-app/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,8 @@
 import { authOptions } from "../../../lib/auth"
 import NextAuth from "next-auth"
 
-export const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions)
 
 // export const GET = handler ;
 // export const POST = handler ;
-export { handler as GET, handler as POST }
+export { handler as GET , handler as POST }
