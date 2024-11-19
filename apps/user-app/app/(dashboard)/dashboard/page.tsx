a2773612e8ddd68bc6a676189947d32ad9f9e117
lucky29-git <chandrashekharchoudha@gmail.com>
2024-11-19 23:50:50 +0530

    dashboard component

--- apps/user-app/app/(dashboard)/dashboard/page.tsx
+++ apps/user-app/app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,10 @@
+import DashboardComponent from "../../../components/DashboardComponent";
 
 
 export default function Dashboard(){
 
     return <div className="ml-20 my-10">
         This is dashboard page ( work on progess ), go to transfer page
+        {/* <DashboardComponent></DashboardComponent> */}
     </div>
 }
